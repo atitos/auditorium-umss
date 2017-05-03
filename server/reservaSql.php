d236a4ec2c50a05783696df1dd60dc705da4d3cd
HenrriYapuraQuispe <henrriyapura@gmail.com>
2017-05-03 19:12:17 -0400

    funcion isertar actualizar

--- server/reservaSql.php
+++ server/reservaSql.php
@@ -6,4 +6,15 @@
     
            mysqli_query($con, "INSERT INTO reserva (IDUSUARIO,IDAMBIENTE,TITULORESERVA,DESCRIPCIONRESERVA,FECHAINICIO, FECHAFIN,HORAINICIO,HORAFIN,SOLICITANTE) VALUES ( $idusuario,$idambiente,'$titulo','$descripcion','$fechainicio','$fechafin','$horainicio','$horafin','$solicitante')");
 	}
+
+	function actualizar($con,$idreserva,$titulo,$descripcion,$fechainicio,$fechafin,$horainicio,$horafin,$solicitante){
+	   
+								  
+    
+          mysqli_query($con, "UPDATE reserva SET TITULORESERVA='$titulo',
+										DESCRIPCIONRESERVA='$descripcion',FECHAINICIO='$fechainicio', FECHAFIN='$fechafin',HORAINICIO ='$horainicio' ,HORAFIN ='$horafin',SOLICITANTE='$solicitante'
+								WHERE IDRESERVA='$idreserva'");
+
+           
+	}
    ?>
